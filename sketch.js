37e5439b768543b07454f1d9f0829181ed803369
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2025-02-12 13:42:32 -0800

    updated starter code to match video

--- sketch.js
+++ sketch.js
@@ -1,15 +1,49 @@
-// Project Title
+// Project Title Shooting Demo
 // Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+
+// player position variables
+let pX = 50;
+let pY = 200;
+
+// projectile positions
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 400);
+  background(240);
 }
 
 function draw() {
-  background(220);
+  background(240);
+  noStroke()
+
+  movePlayer();
+
+  drawPlayer();
+
+  // drawProjectiles();
 }
+
+function drawPlayer(x, y){
+  fill('green');
+  ellipse(x, y, 40, 80);
+}
+
+function movePlayer(){
+  if(keyIsDown(87)){ // w key
+    pY += 5;
+  }
+  if (keyIsDown(83)){ // s key
+    pY -= 5; 
+  }
+}
+
+function keyPressed(){
+  print(keyCode);
+}
+//update and draw projectiles
+function drawProjectiles(){
+  fill('red');
+
+  
+} 
