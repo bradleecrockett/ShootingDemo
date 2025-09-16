4b225e622eb405154fca177497a27db68f897613
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2025-09-16 07:58:56 -0700

    tiny fixes

--- sketch.js
+++ sketch.js
@@ -8,42 +8,51 @@ let pY = 200;
 // projectile positions
 
 
-function setup() {
-  createCanvas(600, 400);
-  background(240);
+function setup()
+{
+    createCanvas(600, 400);
+    background(240);
 }
 
-function draw() {
-  background(240);
-  noStroke()
+function draw()
+{
+    background(240);
+    noStroke()
 
-  movePlayer();
+    movePlayer();
 
-  drawPlayer();
+    drawPlayer();
 
-  // drawProjectiles();
+    // drawProjectiles();
 }
 
-function drawPlayer(x, y){
-  fill('green');
-  ellipse(x, y, 40, 80);
+function drawPlayer(x, y)
+{
+    fill('green');
+    ellipse(x, y, 40, 80);
 }
 
-function movePlayer(){
-  if(keyIsDown(87)){ // w key
-    pY += 5;
-  }
-  if (keyIsDown(83)){ // s key
-    pY -= 5; 
-  }
+function movePlayer()
+{
+    if (keyIsDown(87))
+    { // w key
+        pY += 5;
+    }
+    if (keyIsDown(83))
+    { // s key
+        pY -= 5;
+    }
 }
 
-function keyPressed(){
-  print(keyCode);
+function keyPressed()
+{
+    print(keyCode);
 }
+
 //update and draw projectiles
-function drawProjectiles(){
-  fill('red');
+function drawProjectiles()
+{
+    fill('red');
 
-  
+
 } 
